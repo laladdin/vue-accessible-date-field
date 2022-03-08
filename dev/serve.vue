60d0616977081395ffc3e0677f75c3519e0e3133
Laura Laukkanen <laura.laukkanen@myy.haaga-helia.fi>
2022-03-08 08:51:59 +0200

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import VueAccessibleDateField from '@/vue-accessible-date-field.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueAccessibleDateField
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-accessible-date-field />
+  </div>
+</template>
