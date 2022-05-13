a7933bf9239aeef8fc1320d616e7daac4216c906
laladdin <laura.laukkanen@myy.haaga-helia.fi>
2022-05-13 23:25:37 +0300

    Feature/edit localization (#35)
    * added sv and en
    
    * -

---
+++ src/localizationdefaultdatasv.ts
@@ -0,0 +1,18 @@
+import { Localization } from '@/ilocalization'
+
+export const localizationDefaultDataSv: Localization = {
+  locale: 'sv-SE',
+  placeholderText: 'T.ex. dd.mm.åååå',
+  dateFormatString: 'datumformat: ',
+  wordOrTranslated: 'eller',
+  dateFormatOptions: ['dd.mm.åååå', 'dd-mm-åååå', 'dd/mm/åååå'],
+  generalDateFieldError: 'Datumformatet du angav är ogiltigt',
+  buttonLabelChoose: 'Välj ett datum',
+  buttonLabelChange: 'Ändra det valda datumet, ',
+  dayNames: ['måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag', 'söndag'],
+  monthNames: ['januari', 'februari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december'],
+  monthNamesForMessage: ['januari', 'februari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december'],
+  dayNamesShort: ['må', 'ti', 'on', 'to', 'fr', 'lö', 'sö'], 
+  selectFocusedButtonLabel: 'OK',
+  cancelButtonLabel: 'Avbryt'
+}
