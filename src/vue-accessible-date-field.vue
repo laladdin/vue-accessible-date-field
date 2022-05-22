020fa7847a1c179e31571122d2d0330c763ea7f0
Laura Laukkanen <laura.laukkanen@myy.haaga-helia.fi>
2022-05-22 17:57:06 +0300

    -

--- src/vue-accessible-date-field.vue
+++ src/vue-accessible-date-field.vue
@@ -241,16 +241,16 @@ export default /*#__PURE__*/ defineComponent({
     },
   },
   data() {
-    const showCalendar: boolean = false;
-    const currentMonth: number = new Date().getMonth();
-    const year: number = new Date().getFullYear();
-    var selectedISODate: string | undefined;
-    var selectedDateString: string | undefined;
-    var selectedTdCell: HTMLTableCellElement | undefined;
-    var uniqueString: string | undefined;
+    const showCalendar: boolean = false
+    const currentMonth: number = new Date().getMonth()
+    const year: number = new Date().getFullYear()
+    var selectedISODate: string | undefined
+    var selectedDateString: string | undefined
+    var selectedTdCell: HTMLTableCellElement | undefined
+    var uniqueString: string | undefined
     var errors: string[] = []
-    var selectedDateMessage: string = "";
-    var componentLanguage: string | undefined;
+    var selectedDateMessage: string = ""
+    var componentLanguage: string | undefined
     var localizationData: Localization = {
       locale: "",
       placeholderText: "",
@@ -451,8 +451,8 @@ export default /*#__PURE__*/ defineComponent({
     },
   },
   methods: {
-    updateButtonMessage(newDateValue: string) {
-      const newDate = new Date(newDateValue) 
+    updateButtonMessage(date: string) {
+      const newDate = new Date(date) 
       let dayName = this.localizationData.dayNames[newDate.getDay()]
       let dayNumber = newDate.getDate()
       let monthName = this.localizationData.monthNamesForMessage[newDate.getMonth()]
@@ -462,11 +462,14 @@ export default /*#__PURE__*/ defineComponent({
     checkDisabledDay(dayItem: DayOfMonth): boolean | undefined {
       return dayItem.previousMonthDay || dayItem.nextMonthDay
     },
+    eventPrev(event: Event) {
+      event.stopPropagation()
+      event.preventDefault()
+    },
     closeDatePickerModal(event?: Event): void {
-      this.showCalendar = false
+      this.showCalendar = false      
       if (event) {
-        event.stopPropagation()
-        event.preventDefault()
+        this.eventPrev(event)
       }
       if (this.showCalendar === false) {
         this.year = Number(this.selectedISODate?.split("-")[0])
@@ -480,8 +483,7 @@ export default /*#__PURE__*/ defineComponent({
       oldFocused.tabIndex = newTabIndex
     },
     setFocusToCell(dateString?: string): void {
-      const currentSelected = dateString
-      const tdElement = document.querySelector("[data-date='" + currentSelected + "']") as HTMLTableCellElement
+      const tdElement = document.querySelector("[data-date='" + dateString + "']") as HTMLTableCellElement
       tdElement.focus()
       tdElement.tabIndex = 0               
     },
@@ -491,9 +493,6 @@ export default /*#__PURE__*/ defineComponent({
         return true
       }
       return false
-    },
-    getDateNow(): Date {
-      return new Date()
     },
     setCalendarView(dateString: string): void {
       this.year = Number(dateString.split("-")[0])
@@ -560,19 +559,13 @@ export default /*#__PURE__*/ defineComponent({
           this.setFocusToCell(this.selectedISODate)
         })
       }        
-      if (event) {
-        event.stopPropagation()
-        event.preventDefault()
-      }
+      this.eventPrev(event)
     },
     handleBackdropClick(event: Event): void {
       this.closeDatePickerModal()
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
     },
     handleDatePress(event: Event, item: DayOfMonth, closeModal: boolean): void {
-      event.stopPropagation()
-      event.preventDefault()
       this.selectedTdCell = event.target as HTMLTableCellElement
       this.selectedTdCell.ariaSelected = "true"
       this.selectedTdCell.tabIndex = 0
@@ -583,19 +576,17 @@ export default /*#__PURE__*/ defineComponent({
         this.closeDatePickerModal()
         const icon = document.getElementById("calendarIcon") as HTMLButtonElement
         icon.focus()
-        }
+      }
+      this.eventPrev(event)
     },
-    handleOKButtonClick(event?: Event): void {
+    handleOKButtonClick(event: Event): void {
       const focusedDate = document.querySelector('td[tabindex="0"]') as HTMLTableCellElement
       this.selectedISODate = focusedDate.dataset.date
       const isoString: string = this.selectedISODate!
       this.selectedDateString = this.formatISODate(isoString, ".")
       focusedDate.ariaSelected = "true"
       this.closeDatePickerModal()
-      if (event) {
-        event.stopPropagation()
-        event.preventDefault()
-      }
+      this.eventPrev(event)
     },
     handlePrevYearTab(event: KeyboardEvent): void {
       if (event.shiftKey) {
@@ -611,9 +602,8 @@ export default /*#__PURE__*/ defineComponent({
         event.preventDefault()
       }
     },
-    handlePageDown(event: KeyboardEvent, item: DayOfMonth) {      
-      event.stopPropagation()
-      event.preventDefault()
+    handlePageDown(event: KeyboardEvent, item: DayOfMonth) {    
+      this.eventPrev(event)        
       this.changeTabIndex(0, -1)
       if (event.shiftKey) {
         this.goToPreviousYear()
@@ -630,11 +620,10 @@ export default /*#__PURE__*/ defineComponent({
         const newFocused = document.querySelector("[data-date='" + dateToGoTo + "']") as HTMLTableCellElement
         newFocused.tabIndex = 0
         newFocused.focus()
-      })
+      })      
     },
     handlePageUp(event: KeyboardEvent, item: DayOfMonth) {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       // if also Shift key pressed, picker moves to next year
       if (event.shiftKey) {
@@ -679,8 +668,7 @@ export default /*#__PURE__*/ defineComponent({
       })
 
       if (event) {
-        event.stopPropagation()
-        event.preventDefault()
+        this.eventPrev(event)
       }
     },
     riffleYears(forwardOrBackward: string, event?: Event): void {
@@ -707,8 +695,7 @@ export default /*#__PURE__*/ defineComponent({
       })
 
       if (event) {
-        event.stopPropagation()
-        event.preventDefault()
+        this.eventPrev(event)
       }
     },
     checkIfLeapYear(year: number): boolean {
@@ -755,8 +742,7 @@ export default /*#__PURE__*/ defineComponent({
       }
     },
     goToFirstDayOfWeek(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       var firstDayOfWeek = 0
       const weekdayCurrent = new Date(item.year, item.month, item.day).getDay()
@@ -785,8 +771,7 @@ export default /*#__PURE__*/ defineComponent({
       })
     },
     goToLastDayOfWeek(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       var lastDayOfWeek = item.day
       const weekdayCurrent = new Date(item.year, item.month, item.day).getDay()
@@ -812,8 +797,7 @@ export default /*#__PURE__*/ defineComponent({
       })
     },
     goToPreviousWeek(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       var dayInPreviousWeek = 0
 
@@ -833,8 +817,7 @@ export default /*#__PURE__*/ defineComponent({
       })
     },
     goToNextWeek(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       let dayInNextWeek = 0
 
@@ -857,8 +840,7 @@ export default /*#__PURE__*/ defineComponent({
       })
     },
     goToPreviousDay(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       let previousDay = 0
 
@@ -883,8 +865,7 @@ export default /*#__PURE__*/ defineComponent({
       })
     },
     goToNextDay(item: DayOfMonth, event: Event): void {
-      event.stopPropagation()
-      event.preventDefault()
+      this.eventPrev(event)
       this.changeTabIndex(0, -1)
       let nextDay = 0
 
